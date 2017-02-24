125392d27e6db83180f1452c7452b236b1163788
Rowan Smith <r.smith@internode.on.net>
2017-02-24 19:28:20 +1100

    add support for multiple GPUs

--- neural_style.py
+++ neural_style.py
@@ -126,9 +126,9 @@ def parse_args():
     choices=['avg', 'max'],
     help='Type of pooling in convolutional neural network. (default: %(default)s)')
   
-  parser.add_argument('--device', type=str, 
+  parser.add_argument('--device', nargs='+', 
     default='/gpu:0',
-    choices=['/gpu:0', '/cpu:0'],
+    choices=['/gpu:0', '/gpu:1', '/gpu:2', '/gpu:3', '/cpu:0'],
     help='GPU or CPU mode.  GPU mode requires NVIDIA CUDA. (default|recommended: %(default)s)')
   
   parser.add_argument('--img_output_dir', type=str, 
@@ -565,55 +565,59 @@ def check_image(img, path):
   rendering -- where the magic happens
 '''
 def stylize(content_img, style_imgs, init_img, frame=None):
-  with tf.device(args.device), tf.Session() as sess:
-    # setup network
-    net = build_vgg19(content_img)
-    
-    # style loss
-    if args.style_mask:
-      L_style = sum_masked_style_losses(sess, net, style_imgs)
-    else:
-      L_style = sum_style_losses(sess, net, style_imgs)
-    
-    # content loss
-    L_content = sum_content_losses(sess, net, content_img)
-    
-    # denoising loss
-    L_tv = sum_total_variation_losses(sess, net, init_img)
-    
-    # loss weights
-    alpha = args.content_weight
-    beta  = args.style_weight
-    theta = args.tv_weight
-    
-    # total loss
-    L_total  = alpha * L_content
-    L_total += beta  * L_style
-    L_total += theta * L_tv
-    
-    # video temporal loss
-    if args.video and frame > 1:
-      gamma      = args.temporal_weight
-      L_temporal = sum_shortterm_temporal_losses(sess, net, frame, init_img)
-      L_total   += gamma * L_temporal
+  
+  sess = tf.Session()
 
-    # optimization algorithm
-    optimizer = get_optimizer(L_total)
+  for d in [args.device]:
+    with tf.device(d):
+      # setup network
+      net = build_vgg19(content_img)
+      
+      # style loss
+      if args.style_mask:
+        L_style = sum_masked_style_losses(sess, net, style_imgs)
+      else:
+        L_style = sum_style_losses(sess, net, style_imgs)
+      
+      # content loss
+      L_content = sum_content_losses(sess, net, content_img)
+      
+      # denoising loss
+      L_tv = sum_total_variation_losses(sess, net, init_img)
+      
+      # loss weights
+      alpha = args.content_weight
+      beta  = args.style_weight
+      theta = args.tv_weight
+      
+      # total loss
+      L_total  = alpha * L_content
+      L_total += beta  * L_style
+      L_total += theta * L_tv
+      
+      # video temporal loss
+      if args.video and frame > 1:
+        gamma      = args.temporal_weight
+        L_temporal = sum_shortterm_temporal_losses(sess, net, frame, init_img)
+        L_total   += gamma * L_temporal
 
-    if args.optimizer == 'adam':
-      minimize_with_adam(sess, net, optimizer, init_img, L_total)
-    elif args.optimizer == 'lbfgs':
-      minimize_with_lbfgs(sess, net, optimizer, init_img)
-    
-    output_img = sess.run(net['input'])
-    
-    if args.original_colors:
-      output_img = convert_to_original_colors(np.copy(content_img), output_img)
+      # optimization algorithm
+      optimizer = get_optimizer(L_total)
 
-    if args.video:
-      write_video_output(frame, output_img)
-    else:
-      write_image_output(output_img, content_img, style_imgs, init_img)
+      if args.optimizer == 'adam':
+        minimize_with_adam(sess, net, optimizer, init_img, L_total)
+      elif args.optimizer == 'lbfgs':
+        minimize_with_lbfgs(sess, net, optimizer, init_img)
+      
+      output_img = sess.run(net['input'])
+      
+      if args.original_colors:
+        output_img = convert_to_original_colors(np.copy(content_img), output_img)
+
+      if args.video:
+        write_video_output(frame, output_img)
+      else:
+        write_image_output(output_img, content_img, style_imgs, init_img)
 
 def minimize_with_lbfgs(sess, net, optimizer, init_img):
   if args.verbose: print('\nMINIMIZING LOSS USING: L-BFGS OPTIMIZER')
